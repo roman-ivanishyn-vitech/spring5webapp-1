3957e22716fb4a662f40f2b7f6f90330a1612c85
Ivanishyn <partner.roman.ivanishyn@philips.com>
2021-11-25 17:53:15 +0200

    create two Entities(Book and Author) with ManyToMany Relationship

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5webappApplication {
 
 	public static void main(String[] args) {
-		// create new branch jpaEntities
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
 }
